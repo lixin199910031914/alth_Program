6e5a7fefdb87822b6ad9e624a21a4181d6d2f5e4
jcx <1846439119@qq.com>
2020-01-09 14:02:18 +0800

    banner轮播

--- app/src/main/java/com/wd/health/app/Constant.java
+++ app/src/main/java/com/wd/health/app/Constant.java
@@ -1,6 +1,6 @@
 package com.wd.health.app;
 
 public interface Constant {
-    String BASE_URL ="mobile.bwstudent.com/small/";
+    String BASE_URL ="mobile.bwstudent.com/health/";
     String SUCCESS_CODE ="0000";
 }
