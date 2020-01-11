55dbcdb1f2ce5040084df9d78f53321dd89f61d1
lixin <1763346811@qq.com>
2020-01-11 19:40:17 +0800

    show页面更改

--- app/src/main/java/com/wd/health/adapter/ShowTwoAdapter.java
+++ app/src/main/java/com/wd/health/adapter/ShowTwoAdapter.java
@@ -1,31 +1,57 @@
 package com.wd.health.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.wd.health.R;
+import com.wd.health.model.bean.KeListBean;
+
+import java.util.List;
 
 public class ShowTwoAdapter extends RecyclerView.Adapter <ShowTwoAdapter.TwoHolder>{
+    List<KeListBean.ResultBean> result02;
+    Context context;
+    private View inflate;
+
+    public ShowTwoAdapter(List<KeListBean.ResultBean> result02, Context context) {
+        this.result02 = result02;
+        this.context = context;
+    }
+
+
     @NonNull
     @Override
     public TwoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        inflate = LayoutInflater.from(context).inflate(R.layout.showitem_two_01, viewGroup, false);
+        return new TwoHolder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TwoHolder twoHolder, int i) {
-
+        twoHolder.simpleDraweeVie.setImageURI(result02.get(i).getPic());
+        twoHolder.textView.setText(result02.get(i).getDepartmentName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return result02.size();
     }
 
     class TwoHolder extends RecyclerView.ViewHolder {
 
+        private final SimpleDraweeView simpleDraweeVie;
+        private final TextView textView;
+
         public TwoHolder(@NonNull View itemView) {
             super(itemView);
+            simpleDraweeVie = inflate.findViewById(R.id.show_image_01);
+            textView = inflate.findViewById(R.id.show_name_02);
         }
     }
 }
